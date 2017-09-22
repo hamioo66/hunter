a456feb926a0e8d2ab8afe7eade645f6657fee26
hamioo <986263137@qq.com>
2017-09-22 18:06:54 +0800

    测试

--- Alltest.py
+++ Alltest.py
@@ -29,16 +29,50 @@ print s[:-2]
 
 
 
-
+####
 print random.randint(0,9)
 print random.randrange(0,10)
-
+#######
 i = '13'
 j = str(random.randrange(4, 10)) + ''.join(str(random.choice(range(10))) for j in range(8))
 phoneNum=i+j
 print phoneNum
 
 
-
+########
 import django
 print django.VERSION
+
+#迭代器及for循环
+list=[1,2,3,4,5]
+for x in list:
+    list.pop(0)
+    print(" old: ",str(x))
+    x=1
+    print(" new:",str(x))
+    print(" new list: ",str(list))
+    print()
+
+#循环猜数字游戏
+start=0
+end=100
+number=23
+running=True
+while running:
+    guess=int(raw_input(u'请输入一个1-100范围内的数字'))
+    if guess==number:
+        print u"恭喜你，猜对了！"
+        running=False
+    elif guess<number:
+        global guess1
+        guess1=guess
+        print(u"你猜小了，正确值在 %d " % guess1 + u"到 %d 之间" % end)
+    elif guess>number:
+        global guess2
+        guess2=guess
+        while guess2 < guess1:
+            print(u"你输的数字太大了，正确值在%d" % guess1 + u"到 %d 之间" % guess)
+        else:
+            print(u"你输的数字太大了,正确值在 %d " % guess1 + u"到 %d 之间" % guess2)
+    else:
+        break
